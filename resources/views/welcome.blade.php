d2e7914d97deae8761cea78bcd6ad23057444dfc
Nicola Barbaro <barbaronico93@gmail.com>
2024-08-26 12:22:09 +0200

    Starting the Project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<x-layout>
+    <x-nav />
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="display-1">WELCOME TO PRESTO</h1>
+            </div>
+        </div>
+    </div>
+
+
+
+
+</x-layout>
