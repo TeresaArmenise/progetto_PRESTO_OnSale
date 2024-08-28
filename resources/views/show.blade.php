9e5b698484d35e449e1a4fef10161ba7c1c0ebd3
Nicola Barbaro <barbaronico93@gmail.com>
2024-08-28 10:03:57 +0200

    Modificato Carosello

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -11,7 +11,7 @@
     <div class="container marginCustom">
         <div class="row">
             <div class="col-sm-6">
-                <div id="carousel" class="carouselCustom slide" data-ride="carousel">
+                {{-- <div id="carousel" class="carouselCustom slide" data-ride="carousel">
                     <div class="carousel-inner">
                         <div class="item active">
                             <img src="https://picsum.photos/900">
@@ -61,10 +61,40 @@
                         <a class="right carousel-control" href="#thumbcarousel" role="button" data-slide="next">
                             <span class="glyphicon glyphicon-chevron-right"></span>
                         </a>
-                    </div> <!-- /thumbcarousel -->
-                </div><!-- /clearfix -->
-            </div> <!-- /col-sm-6 -->
-            <div class="col-sm-6">
+                    </div> 
+                </div> --}}
+                
+                <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="https://picsum.photos/900" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://picsum.photos/901" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://picsum.photos/902" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://picsum.photos/903" class="d-block w-100" alt="...">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+                
+                
+            </div> 
+            
+            
+            
+            <div class="col-sm-6 d-flex flex-column justify-content-center align-items-center">
                 <h2>{{$article->title}}</h2>
                 <h4>{{$article->category['name']}}</h4>
                 <p>{{$article->description}}</p>
@@ -73,5 +103,15 @@
             </div> <!-- /col-sm-6 -->
         </div> <!-- /row -->
     </div> <!-- /container -->
+
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <div class="col-6 mt-5 text-start">
+                    <a class="btn colorBtn " href="{{route('home')}}">Torna alla home</a>
+                </div>
+            </div>
+        </div>
+    </div>
     
 </x-layout>
