aca9387fda0bd2b76a17310e659df5d52590b537
Teresa <teresa.armenise@gmail.com>
2024-09-04 17:13:40 +0200

    us8 completata

--- app/Livewire/CreateArticle.php
+++ app/Livewire/CreateArticle.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use App\Models\Article;
 use Livewire\Component;
+use App\Jobs\RemoveFaces;
 use App\Jobs\ResizeImage;
 use Livewire\WithFileUploads;
 use Livewire\Attributes\Validate;
@@ -54,7 +55,14 @@ class CreateArticle extends Component
             foreach ($this->images as $image) {
                 $newFileName = "articles/{$this->article->id}";
                 $newImage = $this->article->images()->create(['path' => $image->store($newFileName, 'public')]);
-                dispatch(new ResizeImage($newImage->path, 300, 300));
+                // vecchio codice
+                // dispatch(new ResizeImage($newImage->path, 300, 300));
+                // nuovo codice
+                RemoveFaces::withChain([
+                    new ResizeImage($newImage->path, 300, 300),
+                    new GoogleVisionSafeSearch($newImage->id),
+                    new GoogleVisionLabelImage($newImage->id),
+                ])->dispatch($newImage->id);
         
             }
             File::deleteDirectory(storage_path('/app/livewire-tmp'));
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,6 +30,27 @@
                     <p class="fst-italic text-dark text-start">{{__('ui.undertitle_carousel_3')}}</p>
                 </div>
             </div>
+            <div class="carousel-item ">
+                <img class="d-block w-100" src="media\abbigliamento.webp" alt="Third slide">
+                <div class="carousel-caption d-flex flex-column justify-content-center pb-5">
+                    <h5 class="text-dark">{{__('ui.title_carousel_4')}}</h5>
+                    <p class="fst-italic text-dark text-start">{{__('ui.undertitle_carousel_4')}}</p>
+                </div>
+            </div>
+            <div class="carousel-item ">
+                <img class="d-block w-100" src="media\movies.webp" alt="Third slide">
+                <div class="carousel-caption d-flex flex-column justify-content-center pb-5">
+                    <h5 class="text-dark">{{__('ui.title_carousel_5')}}</h5>
+                    <p class="fst-italic text-dark text-start">{{__('ui.undertitle_carousel_5')}}</p>
+                </div>
+            </div>
+            <div class="carousel-item ">
+                <img class="d-block w-100" src="media\vino.webp" alt="Third slide">
+                <div class="carousel-caption d-flex flex-column justify-content-center pb-5">
+                    <h5 class="text-dark">{{__('ui.title_carousel_6')}}</h5>
+                    <p class="fst-italic text-dark text-start">{{__('ui.undertitle_carousel_6')}}</p>
+                </div>
+            </div>
         </div>
         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
